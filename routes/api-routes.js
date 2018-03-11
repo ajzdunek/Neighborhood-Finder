aa0ff1e0f245089dae32da2dc13dd127c74c5422
Craig D <dupuiscraig@gmail.com>
2018-03-11 14:15:09 -0500

    adds new post route for login on index and api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,33 +5,37 @@ console.log(db.Neighborhoodgeneral);
 // var neighGen = require("../models/neighborhood_general.js");
 // var neighDeet = require("../models/neighborhood_detail.js");
 
-module.exports = function (app) {
-
-
-  app.get("/api/results/:name", function (req, res) {
+module.exports = function(app) {
+  app.get("/api/results/:name", function(req, res) {
     console.log("req", req.params.name);
-      db.Neighborhoodgeneral.findOne({
+    db.Neighborhoodgeneral.findOne({
+      where: {
+        Name: req.params.name
+      }
+    }).then(function(general) {
+      // console.log("general", general)
+      // res.json(general);
+      db.Neighborhooddetail.findAll({
         where: {
-          Name: req.params.name
+          // need to figure out how to call this on foreign key once tables associated
+          FKNeighborhoodsGen: general.dataValues.id
         }
-  
-      }).then(function (general) {
-        // console.log("general", general)
-        // res.json(general);
-        db.Neighborhooddetail.findAll({
-          where: {
-            // need to figure out how to call this on foreign key once tables associated
-            FKNeighborhoodsGen: general.dataValues.id
-          }
-        }).then(function (details) { 
-            var data = {
-              nameData: general,
-              detailData: details
-            }
-            console.log("details", details);
-            console.log(data);
-            res.json({ data: data })
-          });
-        });
+      }).then(function(details) {
+        var data = {
+          nameData: general,
+          detailData: details
+        };
+        console.log("details", details);
+        console.log(data);
+        res.json({ data: data });
       });
-    };
+    });
+  });
+
+app.post('/api/users', function(req, res) {
+  var userName = req.body.Email;
+  console.log("User's email address is ", userName);
+  res.send(userName);
+});
+
+};
