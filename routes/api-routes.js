f107852080129c8e0064c84765648404fc685a67
Craig D <dupuiscraig@gmail.com>
2018-03-12 14:46:59 -0500

    adds new login post route and api routes for favorite neighborhood

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,49 +12,66 @@ module.exports = function(app) {
       where: {
         Name: req.params.name
       }
-//     }).then(function(general) {
-//       // console.log("general", general)
-//       // res.json(general);
-//       db.Neighborhooddetail.findAll({
-//         where: {
-//           // need to figure out how to call this on foreign key once tables associated
-//           FKNeighborhoodsGen: general.dataValues.id
-//         }
-//       }).then(function(details) {
-//         var data = {
-//           nameData: general,
-//           detailData: details
-//         };
-//         console.log("details", details);
-//         console.log(data);
-//         res.json({ data: data });
-  
-      }).then(function (general) {
-        // console.log("general", general)
-        // res.json(general);
-        db.Neighborhooddetail.findAll({
-          where: {
-            // need to figure out how to call this on foreign key once tables associated
-            NeighborhoodgeneralId: general.dataValues.id
-          }
-        }).then(function (details) { 
-            var data = {
-              nameData: general,
-              detailData: details
-            }
-            console.log("details", details);
-            console.log(data);
-            res.json({ data: data })
-          });
-        });
+    }).then(function(general) {
+      // console.log("general", general)
+      // res.json(general);
+      db.Neighborhooddetail.findAll({
+        where: {
+          // need to figure out how to call this on foreign key once tables associated
+          FKNeighborhoodsGen: general.dataValues.id
+        }
+      }).then(function(details) {
+        var data = {
+          nameData: general,
+          detailData: details
+        };
+        console.log("details", details);
+        console.log(data);
+        res.json({ data: data });
       });
     });
   });
 
-app.post('/api/users', function(req, res) {
-  var userName = req.body.Email;
-  console.log("User's email address is ", userName);
-  res.send(userName);
-});
+  // Check to see if email exists in the db
+  app.get("api/users/:id", function(req, res) {
+    db.Users.findAll({
+      where: {
+        Email: req.body.email
+      }
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
 
+  // Post new email to db
+  // app.post("/api/users", function(req, res) {
+  //   var userName = req.body.Email;
+  //   console.log("User's email address is ", userName);
+  //   res.send(userName);
+  // });
+
+  // POST route for saving a new User
+  app.post("/api/users", function(req, res) {
+    db.Email.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // POST route for saving a new favorite neighborhood    //Double check this
+  app.post("/api/users", function(req, res) {
+    db.Saved.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // PUT route for updating favorite neighborhood
+  app.put("/api/users", function(req, res) {
+    db.Users.update(req.body, {
+      where: {
+        Saved: req.body.Saved
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
 };
