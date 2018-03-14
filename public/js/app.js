5f3349b8472bb4f46b078b4a98efe5d2fd4f4e49
Kelly <kelly.hagan3@gmail.com>
2018-03-13 23:26:40 -0500

    adds error handling to login modal

--- public/js/app.js
+++ public/js/app.js
@@ -89,8 +89,24 @@ $(document).ready(function () {
     //     }
     // })
 
-    $("#submitStart").on("click", saveEmailInfo);
+    function isValidEmailAddress(emailAddress) {
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        return pattern.test(emailAddress);
+        saveEmailInfo();
+    }
 
-
-
+    $("#submitStart").on("click", function(){
+        console.log("running");
+        var email = $("#email").val();
+        console.log(email);
+            if(isValidEmailAddress(email)){
+                if(!$("#submitStart").hasClass('modal-close')){
+                $('#submitStart').addClass('modal-close')
+                    }
+                saveEmailInfo();
+            }else{
+                console.log("error");
+                $("#submitStart").removeClass('modal-close')
+            }
+        })
 });
