844f173cb7c1240192e772d72e167feeca225dd3
Kelly <kelly.hagan3@gmail.com>
2018-03-13 22:06:53 -0500

    put requests working again

--- public/js/app.js
+++ public/js/app.js
@@ -1,88 +1,97 @@
 
 $(document).ready(function () {
 
-        $(".start").on("click", function (e) {
-            e.preventDefault();
-            window.location.href = "/survey.html#quizStart";
-        });
-    
-        console.log("I am working");
-    
-        // Click Login to open modal
-        $('#login-modal').modal();
+    $(".start").on("click", function (e) {
+        e.preventDefault();
+        window.location.href = "/survey.html#quizStart";
+    });
 
-        // When user clicks "submit + start" button, the user's email is saved into the database
-        var checkEmail = function () {
-            console.log("running")
-        
-            var newUser = $("#email").val().trim();
-            console.log("User's email is ", newUser);
-            $.get("/api/users/" + newUser, function (data) {
-                if (data.length > 0 && data[0].Saved !== null) {
-                    var match = data[0].Saved;
-                    saveHoodName(match);
-                    window.location.href = "/survey.html#loadSaved";
-                } else if (data.length > 0) {
+    console.log("I am working");
+
+    // Click Login to open modal
+    $('#login-modal').modal();
+
+    // When user clicks "submit + start" button, the user's email is saved into the database
+    var checkEmail = function () {
+        console.log("running")
+
+        var newUser = $("#email").val().trim();
+        console.log("User's email is ", newUser);
+        $.get("/api/users/" + newUser, function (data) {
+            if (data.length > 0 && data[0].Saved !== null) {
+                var match = data[0].Saved;
+                saveHoodName(match);
+                window.location.href = "/survey.html#loadSaved";
+            } else if (data.length > 0) {
+                window.location.href = "/survey.html#quizStart";
+            }
+            if (data.length <= 0) {
+                $.post("/api/users/" + newUser, function (data) {
                     window.location.href = "/survey.html#quizStart";
-                }
-                if (data.length <= 0){
-                    $.post("/api/users/" + newUser, function (data) {
-                        window.location.href = "/survey.html#quizStart";
-                    });
-                }
-            })
-        }; 
-    
-    
-    
+                });
+            }
+        })
+    };
+
+
+
     // var favorites = JSON.parse(localStorage.getItem("savedplaces"));
     var emailInfo = JSON.parse(localStorage.getItem("savedemail"));
     var savedHood = JSON.parse(localStorage.getItem("savedhood"));
-    
+
     if (!Array.isArray(emailInfo)) {
         emailInfo = [];
     }
-    
+
     if (!Array.isArray(savedHood)) {
-        savedHood= [];
+        savedHood = [];
     }
-    
+
     /** Saves restaurants and corresponding links in local storage 
      * @param {method} event - Prevents page from reloading
      * @return {function} putOnPage - runs putOnPage function
      */
     // function saveRestaurant(event) {
-        function saveEmailInfo(){
+    function saveEmailInfo() {
         emailInfo = [];
         var email = $("#email").val().trim();
         emailInfo.push(email);
         localStorage.setItem("savedemail", JSON.stringify(emailInfo));
         console.log("saved", emailInfo);
         checkEmail();
-    
-        }
-    
+
+    }
+
     /** Saves restaurants and corresponding links in local storage 
      * @param {method} event - Prevents page from reloading
      * @return {function} putOnPage - runs putOnPage function
      */
     // function saveRestaurant(event) {
-        function saveHoodName(hood){
+    function saveHoodName(hood) {
         savedHood = [];
         savedHood.push(hood);
         localStorage.setItem("savedhood", JSON.stringify(savedHood));
         console.log("saved hood", savedHood);
-    
+
         return savedHood;
-        }
-    
-    
-    
+    }
+
+
+
+    // $("#submitStart").on("click", function () {
+    //     if ($("#email").attr('data-error', 'please enter a valid email address')) {
+    //         $("#submitStart").removeClass('modal-close')
+    //         console.log("this email has error")
+    //     }else if($("#email").attr('data-success', 'right')){
+    //         if(!$("#submitStart").hasClass('modal-close')){
+    //             $('#submitStart').addClass('modal-close')
+    //         }
+    //         saveEmailInfo();
+    //     }
+    // })
+
     $("#submitStart").on("click", saveEmailInfo);
-    
-    // $(document).on("click", "#retake", function(){
-    //     quizStart();
-    // });
+
 
 
 });
--- public/js/quiz.js
+++ public/js/quiz.js
@@ -466,6 +466,7 @@ $(document).ready(function () {
         var hoodObj = {
             hood: hoodName
         };
+        console.log(hoodObj);
         neighborhood.push(hoodName);
         localStorage.setItem("savedhood", JSON.stringify(neighborhood));
         
@@ -506,6 +507,7 @@ $(document).ready(function () {
                 $("#neighborhoodName").text(data.data.nameData.Name);
                 $("#neighborhoodName").val(data.data.nameData.Name);
                 $(".description").text(data.data.nameData.Description);
+                $('body').css('background', "url(.." + data.data.nameData.Image + ")");
 
                 $(".mapAppend").html("<img class='map' src='" + data.data.nameData.Map_image + "'>");
                 $(".foodAppend").html("<div class='leftDiv col s12 l4' id='food1'>" + "<a class='link1'>" + data.data.detailData[0].Name + "</a>" + "<p>" + data.data.detailData[0].Description + "</p>" + "<img src='" + data.data.detailData[0].Image + "'>" + "</div>" + "<div class='middleDiv col s12 l4' id='food2'>" + "<a class='link2'>" + data.data.detailData[1].Name + "</a>" + "<p>" + data.data.detailData[1].Description + "</p>" + "<img src='" + data.data.detailData[1].Image + "'>" + "</div>" + "<div class='rightDiv col s12 l4' id='food3'>" + "<a class='link3'>" + data.data.detailData[2].Name + "</a>" + "<p>" + data.data.detailData[2].Description + "</p>" + "<img src='" + data.data.detailData[2].Image + "'>" + "</div>");
